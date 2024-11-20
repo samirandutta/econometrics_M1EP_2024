6bd827f0ced8957c773f834b6e84b9e29df41c71
Samiran Dutta <142884934+samirandutta@users.noreply.github.com>
2024-11-20 11:30:37 +0100

    Update (20.11.24)

--- R_code/Intro5.R
+++ R_code/Intro5.R
@@ -5,16 +5,16 @@ rm(list=ls())
 # PBS6. Q1
 #===========================================================================================================================
 
-#Import data
+#Import data (use your own path!)
 smoke_df <- read.csv("/Users/samirandutta/Library/CloudStorage/Dropbox/Teaching/IMetrics_E&P/IMetrics_2024/Datasets/smoke_data.csv")
 
 # Regress educ on age and get residuals
 m1 <- lm(educ ~ age, data = smoke_df)
-data$res_m1 <- resid(m1)
+smoke_df$res_m1 <- resid(m1)
 
 # Regress cigs on age and get residuals
 m2<- lm(cigs ~ age, data = smoke_df)
-data$res_m2 <- resid(m2)
+smoke_df$res_m2 <- resid(m2)
 
 # FWL method
 m3 <- lm(res_m2 ~ res_m1, data = smoke_df)
@@ -39,11 +39,71 @@ cat("beta_fwl:", beta_fwl, "\n")
 # PBS6. Q2
 #===========================================================================================================================
 
+#Import data (use your own path!)
+bw_df <- read.csv("/Users/samirandutta/Library/CloudStorage/Dropbox/Teaching/IMetrics_E&P/IMetrics_2024/Datasets/birthweight_data.csv")
+
+#Check covariance 
+cov <- cov(bw_df$smoker, bw_df$alcohol)
+cov
+
+# Model without interaction
+r1 <- lm(birthweight ~ smoker + alcohol, data = bw_df)
+summary(r1)
+
+#Model with interaction 
+r2 <- lm(birthweight ~ smoker + alcohol + smoker*alcohol, data = bw_df)
+summary(r2)
 
 
+#===========================================================================================================================
+# PBS6. Q3
+#===========================================================================================================================
 
+#Import data (use your own path!)
+wage_df <- read.csv("/Users/samirandutta/Library/CloudStorage/Dropbox/Teaching/IMetrics_E&P/IMetrics_2024/Datasets/wage1.csv")
 
+#Scatter plot
+scatter <- plot(wage_df$educ, wage_df$wage,
+     main = "",
+     xlab = "Years of Education",
+     ylab = "Hourly Wage",
+     pch = 19,         # Solid circles for points
+     col = "blue")     # Set color for points
+scatter
 
+#Make new varb
+wage_df$educsq <- wage_df$educ^2
+
+#Reg
+reg1 <- lm(wage ~ educ, data = wage_df)
+summary(reg1)
+
+reg2 <- lm(wage ~ educ + educsq, data = wage_df)
+summary(reg2)
+
+# Scatter plot with data points
+plot(wage_df$educ, wage_df$wage,
+     main = "",
+     xlab = "Years of Education",
+     ylab = "Hourly Wage",
+     pch = 19,         # Solid circles for points
+     col = "blue")     # Set color for points
+
+# Add linear regression line (from reg1)
+abline(reg1, col = "grey", lwd = 2, lty = 2)  # Linear regression line
+
+# Add quadratic regression line (from reg2)
+educ_range <- seq(min(wage_df$educ), max(wage_df$educ), length.out = 100)
+educsq_range <- educ_range^2
+predicted_wage <- predict(reg2, newdata = data.frame(educ = educ_range, educsq = educsq_range))
+
+lines(educ_range, predicted_wage, col = "red", lwd = 2)  # Quadratic regression line
+
+# Add legend
+legend("topright", 
+       legend = c("Linear Model", "Quadratic Model"), 
+       col = c("grey", "red"), 
+       lty = c(1, 1))
 
 
 
